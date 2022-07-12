d48bc4de58305fc3c363504b56604e294887a709
yu++ <7052856+yuxx@users.noreply.github.com>
2022-07-12 17:08:41 +0900

    modify: FlutterMethodChannel から呼び出す処理を外出し

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -33,21 +33,10 @@ extension AppDelegate {
   private func flutterMethodCallHandler(call: FlutterMethodCall, result: FlutterResult) -> Void {
     print("\(String(describing: Self.self))::\(#function)@\(#line) channel called."
         + "\ncall.method: \(call.method)")
-
-    let contactVC = CNContactViewController(forNewContact: nil)
-    contactVC.delegate = self
-    let navigationController = UINavigationController(rootViewController: contactVC)
-    navigationController.modalPresentationStyle = .fullScreen
-    flutterVC.present(navigationController, animated: true)
-  }
-}
-
-extension AppDelegate: CNContactViewControllerDelegate {
-  public func contactViewController(_ viewController: CNContactViewController, didCompleteWith contact: CNContact?) {
-    print("\(String(describing: Self.self))::\(#function)@\(#line)"
-        + "\ncontact: \(contact)")
-    // TODO: 保存はこのままネイティブで行うか、回りくどいが Flutter へ戻ってプラグイン経由で保存するなどする。
-    // NOTE: contact が nil の場合はキャンセル
-    viewController.dismiss(animated: true)
+    guard let nativeCallBackType = NativeCallBackType(rawValue: call.method) else {
+      print("\(String(describing: Self.self))::\(#function)@\(#line)")
+      return
+    }
+    nativeCallBackType.nativeProcedure(call, result)
   }
 }
